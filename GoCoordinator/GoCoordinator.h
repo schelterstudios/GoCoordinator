f064d146688da5ce95e513479a9bfb6d4d40e1e1
Steve <steve@schelterstudios.com>
2020-10-17 15:57:03 -0700

    Framework setup

---
+++ GoCoordinator/GoCoordinator.h
@@ -0,0 +1,18 @@
+//
+//  GoCoordinator.h
+//  GoCoordinator
+//
+//  Created by Steve Schelter on 10/17/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoCoordinator.
+FOUNDATION_EXPORT double GoCoordinatorVersionNumber;
+
+//! Project version string for GoCoordinator.
+FOUNDATION_EXPORT const unsigned char GoCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoCoordinator/PublicHeader.h>
+
+
