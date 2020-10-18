4360769c9204be803aed9d0bd072a993b7e5c836
Steve <steve@schelterstudios.com>
2020-10-17 20:18:33 -0700

    setting up unit test/example placeholders

---
+++ GoCoordinatorExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoCoordinatorExamples
+//
+//  Created by Steve Schelter on 10/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
